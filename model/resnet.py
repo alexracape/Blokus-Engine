835849bc105440a882837f04cbf4c9750152acec
Alex Racapé <alex.racape@gmail.com>
2024-07-17 18:38:09 -0400

    Built out resnet and training code

--- model/resnet.py
+++ model/resnet.py
@@ -1,18 +1,19 @@
 import torch
+import torch.nn as nn
 
 
 DIM = 20
 
 
-class ResidualBlock(torch.nn.Module):
+class ResidualBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels):
         super(ResidualBlock, self).__init__()
 
-        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.bn1 = torch.nn.BatchNorm2d(out_channels)
-        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         y = self.conv1(x)
@@ -25,19 +26,43 @@ class ResidualBlock(torch.nn.Module):
         return torch.relu(y)
 
 
-class ResNet(torch.nn.Module):
+class ResNet(nn.Module):
     """ML model that will predict policy and value for game states
-    
-    AlphaZero uses a ResNet architecture for the model with 20 residual 
-    blocks and 256 filters per block. 
+
+    AlphaZero uses a ResNet architecture for the model with 20 residual
+    blocks and 256 filters per block.
+
+    This model uses a resnet backbone with a policy head and a value head.
+    The input to the model is a 20x20x5 tensor where the first 4 channels
+    are binary boards for each player's pieces on the board. The 5th channel
+    is a binary board with the valid moves for the current player.
+    The policy head outputs a probability distribution over the valid moves
+    or the 20x20 spaces. The value head outputs a single value for the expected
+    outcome of the game for each player. The value is between 0 and 1.
     """
 
     def __init__(self, blocks, width):
         super(ResNet, self).__init__()
         self.blocks = blocks
         self.width = width
-        
-        
+
+        self.input = nn.Conv2d(5, width, kernel_size=3, padding=1)
+        self.res_blocks = nn.ModuleList([ResidualBlock(width, width) for _ in range(blocks)])
+        self.policy_head = nn.Sequential(
+            nn.Conv2d(width, 1, kernel_size=1),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        self.value_head = nn.Sequential(
+            nn.Conv2d(width, 1, kernel_size=1),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(DIM * DIM, 4),
+            nn.Tanh(),
+        )
+
 
     def forward(self, boards):
         """Get the policy and value for the given board state
@@ -47,23 +72,19 @@ class ResNet(torch.nn.Module):
         with the valid moves for the current player. For now, I'm just going to use the boards.
         It is unclear why the player color is needed in the state.
         """
-        # print(board.shape)
-        x = self.relu(self.conv1(boards))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
 
-        x = x.view(-1, DIM * DIM)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        # ResNet backbone
+        x = self.input(boards)
+        for block in self.res_blocks:
+            x = block(x)
 
+        # Policy head
         policy = self.policy_head(x)
+        mask = boards[:, 4, :, :].view(boards.size(0), -1)
+        policy = policy * mask
+        policy = torch.softmax(policy, dim=1)
+
+        # Value head
         value = self.value_head(x)
 
-        if len(boards.shape) == 3:
-            mask = boards[4].flatten()
-        else:
-            mask = boards[:, 4, :, :].view(boards.size(0), -1)
-        policy = policy * mask
-
         return policy, value
-
