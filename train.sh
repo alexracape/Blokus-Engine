f3ab2d94649497b7e04e569450dfce4f74dd4ab8
Alex Racapé <alex.racape@gmail.com>
2024-08-07 12:57:17 -0400

    Updated script for cleaner shutdown

--- train.sh
+++ train.sh
@@ -31,11 +31,24 @@ SERVER_PID=$!
 sleep 3
 
 # Start the Rust clients
+CLIENT_PIDS=()
 echo "Starting $NUM_CLIENTS clients..."
 for i in $(seq 1 $NUM_CLIENTS)
 do
     cargo run --bin self_play &
+    CLIENT_PID=$!
+    echo "Client $i started with PID $CLIENT_PID"
+    CLIENT_PIDS+=($CLIENT_PID)
 done
 
-# Wait for all background processes to complete
-wait $SERVER_PID
+# Wait for all client processes to complete
+echo "Waiting for all client processes to complete..."
+for pid in "${CLIENT_PIDS[@]}"; do
+   wait "$pid"
+done
+
+# Once all clients are done, terminate the server
+echo "All client processes completed. Terminating server..."
+terminate
+
+echo "Server terminated. Exiting script."
