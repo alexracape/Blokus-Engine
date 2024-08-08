1112380da85a5f1910c89d6d6bfa57d6255869d6
Alex Racapé <alex.racape@gmail.com>
2024-08-08 17:15:51 -0400

    Major restructuring changes pivoting away from client-server

--- README.md
+++ README.md
@@ -4,11 +4,26 @@
 
 ## Project Outline
 
-This repository contains several main sections. Most of the code is train a neural network using self-play and Monte Carlo Tree Search. During the process you have a neural network that acts as the brains of your players, and you have a game which is progressively getting played out. To simulate a game, all four players use the same network to determine which move they should take. They use MCTS to explore a tree of possible moves, and the game ultimately takes one path down this tree. Once the game is finished, the model can use all of this data it generated to train and improve itself. Then another game can be simulated using the better model to create more data. This is the basic cycle that allows the neural network to improve. To speed things up, you can have one neural network, and you can have multiple games going in parralel at the same time. This means you can batch requests from multiple games and you can generate more data at the same time. This project uses a client-server architecture where the neural network is hosted on a server, and the clients each query the server as they simulate a game. The server code uses Python and Pytorch, and that is all located in the model_server directory. Meanwhile each client uses Rust code in the self_play directory. This code is built on top of the blokus directory which contains all of the core game logic. Lastly, there is a GUI (that currently does not work yet) to play a game against the trained model.
+This repository contains several main sections. Most of the code is train a neural network using self-play and
+Monte Carlo Tree Search. During the process you have a neural network that acts as the brains of your players,
+and you have a game which is progressively getting played out. To simulate a game, all four players use the same
+network to determine which move they should take. They use MCTS to explore a tree of possible moves, and the game
+ultimately takes one path down this tree. Once the game is finished, the model can use all of this data it generated
+to train and improve itself. Then another game can be simulated using the better model to create more data. This
+is the basic cycle that allows the neural network to improve. To speed things up, you can have one neural network,
+and you can have multiple games going in parralel at the same time. This means you can batch requests from multiple
+games and you can generate more data at the same time. This project uses a client-server architecture where the neural
+network is hosted on a server, and the clients each query the server as they simulate a game. The server code uses Python
+and Pytorch, and that is all located in the model_server directory. Meanwhile each client uses Rust code in the self_play
+directory. This code is built on top of the blokus directory which contains all of the core game logic. Lastly, there
+is a GUI (that currently does not work yet) to play a game against the trained model.
 
-## Configuration
+## Training Configuration
 
-All configuration is stored in the environment in the form of environment variables, following the [12 factor app](https://12factor.net/config) methodology. All of the values in this program are listed below, along with an example config file. The total number of games played during training is the number of clients times the number of games per client times the number of training rounds. For example if you have 10 clients, each generating 2 games worth of data per training round, and you train for 1 round, you will have 20 games worth of data. AlphaZero trained on 21 million games of Go.
+All of the configuration for training is done in the model/training.py config class. All of the values in this config are listed below,
+along with an example config file. The total number of games played during training is the number of clients times the number of games
+per client times the number of training rounds. For example if you have 10 clients, each generating 2 games worth of data per training
+round, and you train for 1 round, you will have 20 games worth of data. AlphaZero trained on 21 million games of Go.
 
 | *Variable* | *Description* | *AlphaZero Value* |
 | --- | --- | --- |
@@ -33,60 +48,21 @@ All configuration is stored in the environment in the form of environment variab
 | EXPLORATION_FRAC | Fraction used to mix noise and prior probability | 0.25 |
 
 
-#### Example .env file:
-
-```env
-PORT=8082
-SERVER_URL=http://[::1]:8082
-TRAINING_ROUNDS=1
-
-# -- Server --
-BUFFER_CAPACITY=1000
-LEARNING_RATE=0.01
-BATCH_SIZE=128
-BATCHING_FREQUENCY=0.01
-TRAINING_STEPS=10
-NN_WIDTH=32
-NN_BLOCKS=2
-
-# -- Client --
-NUM_CLIENTS=10
-GAMES_PER_CLIENT=1
-SIMS_PER_MOVE=2
-SAMPLE_MOVES=30
-C_BASE=19652
-C_INIT=1.25
-DIRICHLET_ALPHA=0.3
-EXPLORATION_FRAC=0.25
-CHECK_INTERVAL=2
-```
-
 ## Usage:
+
+### GUI
 
 To open the GUI in the browser run the model server and the proxy server then run:
 `cd gui`
 `trunk serve --open`
 
-To run the proxy server:
-`envoy -c proxy/envoy.yaml`
+### Training
 
-To run server:
-`python model/model_server.py`
+To train on the HPC, run:
 
-To run self_play client:
-`cargo run --bin self_play`
+`sbatch -N 1 -n 8 train.sh`
 
-### Development
-
-To generate server gRPC code: `python -m grpc_tools.protoc -Iproto --python_out=./model --pyi_out=./model --grpc_python_out=./model ./proto/model.proto`
-
-### Docker
-
-To train the model using docker: `docker compose up`
-
-To copy model X from the running server: `docker cp <container-id>:/server/models/model_X.pt /destination/path`
-
-To copy training data from the running server: `docker cp <container-id>:/server/data/training_stats.csv /destination/path`
+-N is the number of computer nodes and -n is the number of CPUs
 
 
 ## References
---
+++ model/training.py
@@ -0,0 +1,224 @@
+import multiprocessing as mp
+import logging
+import os
+from queue import Empty
+
+import numpy as np
+import pandas as pd
+import torch
+from torchrl.data import ReplayBuffer, LazyTensorStorage
+from tensordict import tensorclass
+
+from blokus_self_play import play_game
+from resnet import ResNet
+
+DIM = 20
+STATS_PATH = "./data/training_stats.csv"
+MODEL_PATH = "./models"
+
+@tensorclass
+class Data:
+    states: torch.Tensor
+    policies: torch.Tensor
+    scores: torch.Tensor
+
+
+def process_inference_batches(model, device, inference_queue, pipes_to_workers):
+    """Process batches of inputs from the self-play games"""
+
+    while True:
+        try:
+            # TODO: Pull from here and process batch in one go
+            idx, inputs = inference_queue.get(timeout=1)  # Get input from the queue
+            inputs = torch.tensor(inputs).to(device)
+            with torch.no_grad():
+                batch = torch.stack(requests)
+                outputs = model(inputs)
+            result_queue.put((idx, outputs.cpu().numpy()))  # Put result in the result queue
+        except Empty:
+            continue
+
+
+def save(game, buffer: ReplayBuffer,):
+    """Save the game data to the replay buffer"""
+
+    logging.info(f"Saving game: {game}")
+
+    # Allocate space for the data
+    history, policies, values = game
+    num_moves = len(game)
+    states = torch.zeros(num_moves, 5, DIM, DIM, dtype=torch.float32)
+    policies = torch.zeros(num_moves, DIM * DIM, dtype=torch.float32)
+    scores = torch.tensor(values, dtype=torch.float32).repeat(num_moves, 1)
+
+    # For each move from this game, update the state and policy
+    new_state = torch.zeros(5, DIM, DIM, dtype=torch.float32)
+    for i, (move, policy) in enumerate(zip(history, policies)):
+
+        # Keep running track of state in new_state
+        player, tile = move.player, move.tile
+        row, col = tile // DIM, tile % DIM
+        new_state[player, row, col] = 1
+
+        # Shift the state to the correct player's perspective
+        states[i] = torch.cat((new_state[player:], new_state[:player]), dim=0)
+
+        # Update the policy for this move
+        for element in policy:
+            action, prob = element
+            policies[i, action] = prob
+
+            # Update which squares are legal on this move
+            row, col = action // DIM, action % DIM
+            states[i, 4, row, col] = 1
+
+    data = Data(
+        states = states,
+        policies = policies,
+        scores = scores,
+        batch_size = [num_moves]
+    )
+    buffer.extend(data)
+
+
+def train(step, model, buffer, optimizer, policy_loss, value_loss, device, stats):
+    """Train the model on a batch of data from the replay buffer"""
+    logging.info(f"Training step: {step}")
+
+    # Get a batch of data from the replay buffer
+    batch = buffer.sample()
+    inputs = batch.get("states").to(device)
+    policies = batch.get("policies").to(device)
+    values = batch.get("scores").to(device)
+
+    # Train the model
+    optimizer.zero_grad()
+    policy, value = model(inputs)
+    policy_loss = policy_loss(policy, policies)
+    value_loss = value_loss(value, values)
+    loss = policy_loss + value_loss
+    loss.backward()
+    optimizer.step()
+
+    # Store training statistics
+    row = pd.DataFrame([{
+        "round": step,
+        "loss": loss.item(),
+        "value_loss": value_loss.item(),
+        "policy_loss": policy_loss.item(),
+        "buffer_size": len(buffer)
+    }])
+    if stats.empty:
+        stats = row
+    else:
+        stats = pd.concat([stats, row])
+    stats.to_csv(STATS_PATH)
+
+
+def main(num_cpus):
+    """Train the model
+
+    Creates the model then spawns multiple processes to generate
+    training data through self-play. The training data is then used
+    to train the model. This process is repeated until the model
+    reaches a certain number of training steps.
+    """
+
+    # Load environment variables
+    config = Config()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f"Using device: {device}")
+
+    # Create the model, optimizer, and loss
+    model = ResNet(config.nn_depth, config.nn_width)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    policy_loss = torch.nn.CrossEntropyLoss().to(device)
+    value_loss = torch.nn.MSELoss().to(device)
+
+    # Set up replay buffer
+    buffer = ReplayBuffer(
+        storage=LazyTensorStorage(config.buffer_capacity),
+        batch_size=config.batch_size
+    )
+
+    # Set up queues and multiprocessing for self-play
+    inference_queue = mp.Queue()
+    pipes_to_model = []
+    pipes_to_workers = []
+    for i in range(config.num_workers):
+        a, b = mp.Pipe()
+        pipes_to_model.append(a)
+        pipes_to_workers.append(b)
+    inference_process = mp.Process(target=process_inference_batches, args=(model, device, inference_queue, pipes_to_workers))
+    inference_process.start()
+
+    # Set up stats for tracking training progress
+    stats = pd.DataFrame(columns=["round", "loss", "value_loss", "policy_loss", "buffer_size"])
+    training_round = 0
+    num_saves = 0
+
+    # Train the model
+    training_round = 0
+    while training_round < config.training_rounds:
+
+        # Generate training data through self-play
+        with mp.Pool(config.num_workers) as pool:
+            game_data = pool.starmap(
+                play_game,
+                [(id, config, inference_queue, pipes_to_model[id]) for id in range(config.games_per_worker)]
+            )
+
+        # Save the game data to the replay buffer
+        for game in game_data:
+            save(game, buffer)
+
+        # Train the model
+        for step in range(config.training_steps):
+            train(step, model, buffer, optimizer, policy_loss, value_loss, device, stats)
+        torch.save(model, f"{MODEL_PATH}/model_{training_round}.pt")
+        training_round += 1
+
+
+class Config:
+    """Configuration for training the model
+
+    AlphaZero used the following values for training:
+        buffer_capacity = 1000000 games
+        learning_rate = 0.01 -> 0.0001 with a scheduler
+        batch_size = 2048
+        training_steps = 700000
+        num_workers = 5000
+        sims_per_move = 800
+        sample_moves = 30
+        c_base = 19652
+        c_init = 1.25
+        dirichlet_alpha = 0.3
+        exploration_fraction = 0.25
+    """
+
+    def __init__(self, num_cpus=4):
+        self.training_rounds = 1
+        self.buffer_capacity = 10000
+        self.learning_rate = 0.01
+        self.batch_size = 256
+        self.inference_interval = .01  # seconds
+        self.training_steps = 10
+        self.num_workers = num_cpus - 1
+        self.games_per_worker = 1
+        self.rounds = 1
+        self.nn_width = 256
+        self.nn_depth = 2
+        self.sims_per_move = 16
+        self.sample_moves = 30
+        self.c_base = 19652
+        self.c_init = 1.25
+        self.dirichlet_alpha = 0.3
+        self.exploration_fraction = 0.25
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    num_cpus = int(os.environ.get("SLURM_NPROCS", 1))
+    logging.info(f"Number of CPUs: {num_cpus}")
+
+    main(num_cpus)
