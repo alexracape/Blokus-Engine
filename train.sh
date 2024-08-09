28d1cae00c1e5ce876f6deda459eae2192e87ac7
Alex Racapé <alex.racape@gmail.com>
2024-08-09 14:00:39 -0400

    Added base_venv for HPC environment

--- train.sh
+++ train.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-python -m venv venv
-source venv/bin/activate
-pip install -r model/requirements.txt
-
-cd self_play
-maturin develop
-cd ..s
+source base_venv/bin/activate
+pip install -r requirements.txt
 
 python model/training.py -n $SLURM_NPROCS
