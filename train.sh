143fac654ddfa8d3956042b9378f00d65376b5b4
Alex Racapé <alex.racape@gmail.com>
2024-08-09 14:52:34 -0400

    Fixed train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-source base_venv/bin/activate
+python -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
 python model/training.py -n $SLURM_NPROCS
