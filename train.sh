898d5b49395eab9420807cc43c744b221c1614b3
Alex Racapé <alex.racape@gmail.com>
2024-08-09 12:52:35 -0400

    Fixed script and requirements to take out maturin generated packages

--- train.sh
+++ train.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END,FAIL
 
+python -m venv venv
+source venv/bin/activate
+pip install -r model/requirements.txt
+
+cd self_play
+maturin develop
+cd ..s
+
 python model/training.py -n $SLURM_NPROCS
